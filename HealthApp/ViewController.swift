f38f464649ef02b8d438637524e34b3c3b57b0e5
saadetsimsek <saadetsimsek13@gmail.com>
2025-02-24 14:46:22 +0300

    Initial Commit

---
+++ HealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by Saadet Şimşek on 24/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HealthAppTests/HealthAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppTests.swift
+//  HealthAppTests
+//
+//  Created by Saadet Şimşek on 24/02/2025.
+//
+
+import Testing
+@testable import HealthApp
+
+struct HealthAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
